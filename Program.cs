cd7d9b8c99f421b10a2b20df44b7596bc6522450
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-01-16 12:45:00 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringKlase3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisi Recenicu:");
+            string s = Console.ReadLine();  
+            
+            string sVelika = s.ToUpper();
+            string sMala = s.ToLower();
+            string sPrvaTri = s.Substring(0,2);
+            string sZadnjihPet = s.Substring(s.Length-5);
+            string s8_11 = s.Substring(7,10);
+
+            Console.WriteLine(sVelika);
+            Console.WriteLine(sMala);   
+            Console.WriteLine(sPrvaTri);
+            Console.WriteLine(sZadnjihPet);
+            Console.WriteLine(s8_11);
+
+            Console.ReadKey();
+        }
+    }
+}
